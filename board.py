ee44469493f79588c18908a22548157ae4a4a980
koenkeune <koenkeune@live.nl>
2020-12-07 18:17:00 +0100

    makeMove now moves the furthest pawn (not tested)

--- board.py
+++ board.py
@@ -3,35 +3,57 @@ from random import randrange
 
 # each player plays according to a certain strategy
 # each player has 4 pawns of which each have a spot
+# the pawns have a relative position from the player pov
 class Player:
-    def __init__(self, strategy, startingPoint, endPoint):
-            self.strategy = strategy
-            self.startingPoint = startingPoint
-            self.endPoint = endPoint
-            numberOfPawns = 4
-            self.pawns = [-1 for i in range(numberOfPawns)] # start outside the board
+    def __init__(self, number, strategy, startingPoint, endPoint):
+        self.name = 'player' + str(number)
+        self.strategy = strategy
+        self.startingPoint = startingPoint
+        self.endPoint = endPoint
+        numberOfPawns = 4
+        self.pawns = {}
+        for i in range(numberOfPawns):
+            self.pawns['pawn' + str(i)] = -1 # not on the board yet  
+    
+    # should still check if there are pawns left
+    def makeMove(self, diceNumber): # not sure if i should add board, keep everything in player, or keep everything in board
+        maxValue = max(self.pawns.values())
+        furthestPawn = list(self.pawns.keys())list(self.pawns.values()).index(maxValue)
+        
+        
+        
+        if pawns[furthestPawn] != -1:
+            self.pawns[furthestPawn] += diceNumber
+        elif diceNumber == 5:
+            self.pawns[furthestPawn] = 0
             
-    
-    
-    def makeMove(self): # not sure if i should add board, keep everything in player, or keep everything in board
-        diceNumber = randrange(1,7)
-        
-        availableMoves = []
+        if self.pawns[furthestPawn] > 68:
+            del self.pawns[furthestPawn]
         
         
-        self.pawns[0] = self.startingPoint
+        #pawnsInBase = [i for i,j in pawns.items() if j == -1]
+        
+        # for pawn in pawnsToMove:
+            
+        
+        # availableMoves = []
+        
+        
+        
+        
+        # pawnName = pawnsToMove[0]
+        # board[self.pawns[pawnName]] = self.name + '_' + pawnName
+        # self.pawns[pawnName] = self.startingPoint
+         
         
     #def availableMoves(self, diceNumber):
-        
-    
-    
     
     #def performStrategy(self, strategy):
     
     #def getFurthestPawn():
 
 
-###### initialize board
+# initialize board
 
 # the board keeps track where all the pawns of each player are
 def board(): # now only the small board variant
@@ -48,10 +70,8 @@ def board(): # now only the small board variant
         boardWithoutPlayers[endPoint] = 'entrance spot' # and safe point
         boardWithoutPlayers[startingPoint] = 'starting point '
         boardWithoutPlayers[i * 17 + 12 - 1] = 'safe spot'
-        players.append(Player('random', startingPoint, endPoint))
+        players.append(Player(i, 'random', startingPoint, endPoint))
         
-    
-    #########################
 
     return(boardWithoutPlayers, players)
     
--- game.py
+++ game.py
@@ -4,18 +4,22 @@ from random import randrange
     
 def game(board, players):
     # while...
+    print(board)
     sixesThrown = 0
     # for each player...
-    diceNumber = players[0].makeMove()
+    diceNumber = randrange(1,7)
     if diceNumber == 6 & sixesThrown == 2:
         print('pass') # pass
     elif diceNumber == 6:
         sixesThrown += 1
         
+    board = players[0].makeMove(board, sixesThrown)
+    
+        
     print(players[0].pawns)
     print(players[1].pawns)
         
-        
+    print(board)
     
  
     
